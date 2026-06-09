cdfeb2be9928fd549be6dc12cd336fdf541a0f5f
Sharon Moegi <sharamoegi@gmail.com>
2026-06-09 23:07:29 +0300

    Initialize attributes

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount=0):
+        # Initialize attributes
+        self.total = 0.0
+        self.items = []
+        self.previous_transactions = []
